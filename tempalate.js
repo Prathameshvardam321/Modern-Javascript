feae606052c88e3780e298d8cde92db15fbb6c59
Prathameshvardam321 <prathameshvardam321@gmail.com>
2023-01-19 23:27:33 +0530

    Added template strings

---
+++ tempalate.js
@@ -0,0 +1,9 @@
+const data = {
+    name: "Prathmesh",
+    age: 21,
+    born: "India"
+}
+const { born, name, age } = data
+
+const intro = `I am ${name} with age ${age} born in ${born}`
+console.log(intro);
